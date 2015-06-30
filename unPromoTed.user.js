921c9340a87f0fa092236c1094eea8e6ecef5236
brianboyko <brian.boyko@gmail.com>
2015-06-30 09:21:07 -0500

    works

--- unPromoTed.user.js
+++ unPromoTed.user.js
@@ -1,31 +1,41 @@
+// unPromoTed.user.js
+
+
+
+console.log("loading");
+
 var findTed = function() {
     var isTed = false;
+
     var metas = document.getElementsByTagName('meta');
     for (i = 0; i < metas.length; i++) {
         if (metas[i].getAttribute("content") == "promoted") {
             isTed = true;
         };
     };
+    console.log("isTed:" + isTed)
     return isTed;
 };
 
 var killTed = function() {
-    document.getElementById('image').innerHTML =
+    document.getElementById('image').innerHTML = 
         'Promoted Content Removed by unPromotTed.';
+
+            console.log("killed Ted");
 };
 
 var runScript = function() {
     console.log("ready");
     var isTed = findTed();
     console.log(isTed);
-    if (isTed || isImgur) {
+    if (isTed) {
         killTed();
     };
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-        if (window.location.href.indexOf("imgur.com") > -1) {
-            runScript();
-        };
-});
 
+
+if (window.location.href.indexOf("imgur.com") > -1) {
+    console.log("running script");
+    runScript();
+};
